415c9b52bf7227fab2555178b50b94f00b7cdada
Harry <hjwp2@cantab.net>
2025-10-23 11:50:47 +0100

    Add js version of scheme for fact

--- spec/JsSchemeSpec.js
+++ spec/JsSchemeSpec.js
@@ -1,9 +1,24 @@
 const jss = require('../lib/jsscheme');
 
+const fact = [
+    "define",
+    "fact",
+    [
+        "lambda",
+        ["n",],
+        [
+            "if",
+            ["=", "n", 1],
+            1,
+            ["*", "n", ["fact", ["-", "n", 1]]],
+        ],
+    ],
+]
+
 describe('Interpreter', function() {
 
   it('should return primitives as themselves', function() {
-    result = jss.eval(42)
+    result = jss.eval(42);
     expect(result).toEqual(42);
   });
 
